ba491dd3f1dda5f6cc981f77b42d8e00fb0aa891
selfcac <selfcac@5gbinbox.com>
2019-02-26 22:26:38 +0200

    http filter module

---
+++ HTTPProtocolFilter/IHTTPFilter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPProtocolFilter
+{
+    public enum BlockPhraseType // CONTENT \ EP
+    {
+        CONTAIN = 0, WORD, REGEX
+    }
+
+    public enum AllowEPType //  EP
+    {
+        CONTAIN = 0, STARTWITH, REGEX
+    }
+
+    public enum AllowDomainType // DOMAIN
+    {
+        EXACT = 0, SUBDOMAINS
+    }
+
+    public class PhaseFilter
+    {
+        public BlockPhraseType Type;
+        public string Phrase;
+    }
+
+    public class AllowEP
+    {
+        public AllowEPType Type;
+        public string EpFormat;
+    }
+
+    public class AllowDomain
+    {
+        public AllowDomainType Type;
+        public string DomainFormat;
+    }
+
+    public interface IHTTPFilter
+    {
+        void BlockPhrase(PhaseFilter phase);
+        void WhitelistEpOfDomain(AllowDomain domain, List<AllowEP> EPs);
+        void allowEntireDomain(AllowDomain domain);
+
+        void ReloadPolicy(string filename);
+        void SavePolicy(string filename);
+    }
+}
