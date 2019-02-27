46db6fdd93497ef41448a02fee27805445faf5a4
selfcac <selfcac@5gbinbox.com>
2019-02-28 00:04:22 +0200

    more test to solidify our algo! *MUST* use dot before domain in DomainFormat o.w. test fail as subdomains-allowed cause troubles.

--- HTTPProtocolFilterTests/Utils/TrieTests.cs
+++ HTTPProtocolFilterTests/Utils/TrieTests.cs
@@ -21,6 +21,21 @@ namespace HTTPProtocolFilter.Utils.Tests
             {
                 "reddit.com",  ".red.com"
             });
+
+            AllowDomain ad = new AllowDomain()
+            {
+                Type = AllowDomainType.SUBDOMAINS,
+                DomainFormat = ".gogo.com",
+                WhiteListEP = new List<AllowEP>()
+                 {
+                     new AllowEP()
+                     {
+                         Type = AllowEPType.CONTAIN,
+                         EpFormat = "Contain"
+                     }
+                 }
+            };
+            t.InsertDomain(ad);
             
 
             Assert.AreNotEqual(null, t.PostfixDomain("go.red.com"));
@@ -36,6 +51,11 @@ namespace HTTPProtocolFilter.Utils.Tests
             Assert.AreNotEqual(null, t.CheckDomain("go.red.com"));
             Assert.AreNotEqual(null, t.CheckDomain("a.b.c.d.go.red.com"));
             Assert.AreEqual(null, t.CheckDomain("hack-red.com"));
+
+            Assert.AreEqual(null, t.CheckDomain("rrrred.com"));
+            Assert.AreEqual(null, t.CheckDomain("rrrreddit.com"));
+
+            Assert.AreEqual(AllowEPType.CONTAIN, t.CheckDomain("go.gogo.com").Tag.WhiteListEP[0].Type);
         }
 
        
