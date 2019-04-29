8fcea20ef84d90b331021e93013ecf860d30c523
selfcac <selfcac@5gbinbox.com>
2019-04-29 22:50:54 +0300

    adding DllExport with "DllExport -action Configure"

--- HTTPProtocolFilter/FilterPolicy.cs
+++ HTTPProtocolFilter/FilterPolicy.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -65,6 +66,7 @@ namespace HTTPProtocolFilter
 
         public List<PhraseFilter> BlockedPhrases = new List<PhraseFilter>();
 
+        [DllExport]
         public static List<string> getWords(string text)
         {
             if (string.IsNullOrEmpty(text))
