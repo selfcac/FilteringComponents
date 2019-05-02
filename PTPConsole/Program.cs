63da3134392b3d5242be1978e12744869ad96c43
selfcac <selfcac@5gbinbox.com>
2019-05-02 22:54:05 +0300

    Process protection from termination

---
+++ PTPConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using ProcessTerminationProtection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PTPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                foreach(string a in args)
+                {
+                    int processid = 0;
+                    if (int.TryParse(a, out processid))
+                    {
+                        Console.WriteLine("Trying to protect id: " + processid);
+                        ProcessProtect.ProtectFromUsersByID(processid);
+                    }
+                }
+                Console.WriteLine("[DONE]");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+    }
+}
